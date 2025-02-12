ca6b55acb6f6100896f37976f622419c334351dd
lefteris alexakis <lefteris.alexakis@kpn.com>
2025-02-12 04:43:20 +0100

    fix: make firewall and network route optional

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 locals {
-  ubuntu_version = "ubuntu-22.04"
+  ubuntu_version = "ubuntu-24.04"
 }
 
 resource "hcloud_network" "private" {
@@ -16,6 +16,8 @@ resource "hcloud_network_subnet" "private" {
 }
 
 resource "hcloud_network_route" "default" {
+  count = var.deploy_nat_gateway ? 1 : 0
+
   network_id  = hcloud_network.private.id
   destination = "0.0.0.0/0"
   gateway     = var.nat_gateway_ip
@@ -23,5 +25,7 @@ resource "hcloud_network_route" "default" {
 
 
 resource "hcloud_firewall" "nat_gateway" {
+  count = var.deploy_nat_gateway ? 1 : 0
+
   name = "nat-gateway"
 }
